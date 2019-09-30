f5481a1a35ee54bd5c5d36c9c5d99e343f7034fd
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-30 01:08:45 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,7 +19,7 @@ attr_accessor :name, :yrs_experience
     Meal.new(self, customer, total, tip)
   end
   
-  def def new_meal(waiter, total, tip=0)
+ def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
   
